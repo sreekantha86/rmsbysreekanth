943da2c462f8b00599952c054184bcdec3fccee1
sreekanth-ihits <sreekanth@ihitsindia.com>
2017-12-19 11:42:07 +0530

    SQLite connection class created

---
+++ RigRepository/SQLiteConnection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SQLite;
+
+namespace RigRepository
+{
+    public class SQLiteConnection
+    {
+        public SQLiteConnection()
+        {
+            
+        }
+        public void CreateDatabase()
+        {
+            
+        }
+    }
+}
